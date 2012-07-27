959e58d0767d30285138b07674be970ad78a1753
Javier Arce <javierarce@gmail.com>
2012-07-27 14:02:41 +0200

    Changes img path

--- config.rb
+++ config.rb
@@ -14,7 +14,7 @@ output_style = :compressed
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
