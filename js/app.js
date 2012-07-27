6748623057a821fa90f7fc9009c169763885f94c
Javier Arce <javierarce@gmail.com>
2012-07-27 15:36:07 +0200

    Cleaned up the code a little bit

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ config   = {
   username: 'viz2',
   lat:  34,
   lng:  35,
-  zoom: 6
+  zoom: 6,
+  template: 'http://{S}tiles.mapbox.com/v3/cartodb.map-byl8dnag/{Z}/{X}/{Y}.png'
 };
 
 var CartoDB = Backbone.CartoDB({ user: config.username });
@@ -15,32 +16,44 @@ var ConflictMap = CartoDB.CartoDBModel.extend({
   ANIMATION_TIME: 3600*4*100000,
 
   getPos: function() {
+
     var coords = $.parseJSON(this.get('position')).coordinates;
+
     return new MM.Location(coords[1], coords[0]);
+
   },
+
   isActive: function(t) {
+
     var dt = t - this.time.getTime();
+
     return dt > 0 && dt < this.ANIMATION_TIME;
+
   },
+
   scaleAt: function(t) {
     var dt = t - this.time.getTime();
     var interpol_time = this.ANIMATION_TIME;
-    if(dt > 0 && dt < interpol_time) {
+    if (dt > 0 && dt < interpol_time) {
       var tt = this.scale*dt/interpol_time;
       var r = 1 + 15*Math.log(tt+1)*0.7;
       return r;
     }
     return 0;
   },
+
   opacity: function(t) {
-    var dt = t - this.time.getTime();
-    var interpol_time = this.ANIMATION_TIME*1.2;
-    if(dt > 0 && dt < interpol_time) {
-      var a= (1 - dt/interpol_time);
-      return Math.max(0, a*a)*0.5;
+    var
+    dt            = t - this.time.getTime(),
+    interpol_time = this.ANIMATION_TIME*1.2;
+
+    if (dt > 0 && dt < interpol_time) {
+      var a = (1 - dt/interpol_time);
+      return Math.max(0, a*a) * 0.5;
     }
     return 0;
   }
+
 });
 
 var ConflictMaps = CartoDB.CartoDBCollection.extend({
@@ -50,7 +63,7 @@ var ConflictMaps = CartoDB.CartoDBCollection.extend({
     this.bind('reset', this.transform);
   },
 
-  // transform the data and prepare some needs interpolations
+  // Transform the data and prepare some needs interpolations
   transform: function() {
     this.each(function(m) {
       m.time = new Date(m.get('timestamp'));
@@ -62,64 +75,80 @@ var ConflictMaps = CartoDB.CartoDBCollection.extend({
   },
 
   getDeathToll: function(t) {
-    var toll = [];
-    var inactive = [];
+    var
+    toll     = [],
+    inactive = [];
+
     this.each(function(m) {
-      //if(m.isActive(t)) {
       toll.push([new Date(m.get('timestamp')).getTime(),m.get('toll'), m.get('child_deaths')]);
-      //}
     });
+
     return toll;
   },
+
   getActiveStatus: function(t) {
-    var active = [];
-    var inactive = [];
+    var
+    active   = [],
+    inactive = [];
+
     this.each(function(m) {
-      if(m.isActive(t)) {
+
+      if (m.isActive(t)) {
         active.push({ 'id': m.id , 'data': m });
       } else {
         inactive.push({ 'id': m.id , 'data': m });
       }
+
     });
+
     return {'active':active,'inactive':inactive};
   },
 
   model: ConflictMap,
-  sql: "SELECT event at time zone 'EDT' as timestamp, ST_AsGeoJson(the_geom) as position, toll as toll, cartodb_id as id, child_deaths, females FROM syrianspring ORDER BY event ASC"
+  sql: "SELECT event at time zone 'EDT' AS timestamp, ST_AsGeoJson(the_geom) AS position, toll AS toll, cartodb_id as id, child_deaths, females FROM syrianspring ORDER BY event ASC"
 
 });
 
 
 /*
-* animated overlay
+* Animated overlay
 */
 function Overlay(map, conflictmaps) {
 
   this.conflictmaps = conflictmaps;
+
   this.time = conflictmaps.first().time.getTime();
+
+  // Create container for the graph
   this.div = document.createElement('div');
   this.div.style.position = 'absolute';
-  this.div.style.width =  map.dimensions.x + "px";
-  this.div.style.height = map.dimensions.y + "px";
+  this.div.style.width    =  map.dimensions.x + "px";
+  this.div.style.height   = map.dimensions.y + "px";
+
   map.parent.appendChild(this.div);
+
   this.svg = d3.select(this.div).append("svg:svg")
   .attr("width",  map.dimensions.x)
   .attr("height", map.dimensions.y);
 
   var self = this;
+
   var callback = function(m, a) {
     return self.draw(m);
   };
+
   this.setTime = function(n){
-    if( self.conflictmaps.last().time.getTime() < self.time){
+
+    if ( self.conflictmaps.last().time.getTime() < self.time){
       self.time = self.conflictmaps.first().time.getTime();
     } else{
       self.time = self.time+n;
     }
+
     clock.set(new Date(self.time));
     var dtt = new Date(self.time);
-    //console.log(dtt.toString());
   }
+
   map.addCallback('drawn', callback);
   this.draw(map);
 
@@ -127,15 +156,22 @@ function Overlay(map, conflictmaps) {
 
 Overlay.prototype = {
   graph: function(divid){
-    var startTime = this.conflictmaps.first().time.getTime();
-    var endTime = this.conflictmaps.last().time.getTime();
-    var data = this.conflictmaps.getDeathToll(endTime+10000);
-    var timeStep = 300;
-    var m = [0, 0, 0, 0]; // margins
-    var w = 164 - m[1] - m[3];	// width
-    var h = 120 - m[0] - m[2]; // height
-    var x = d3.time.scale().domain([startTime, endTime]).range([0, w]);
-    var y = d3.scale.linear().domain([0, d3.max(data, function(d) { return d[1]; })]).range([h, 0]);
+
+    var
+    startTime = this.conflictmaps.first().time.getTime(),
+    endTime   = this.conflictmaps.last().time.getTime(),
+    data      = this.conflictmaps.getDeathToll(endTime+10000),
+    timeStep  = 300;
+
+    var // margins and dimensions
+    m = [0, 0, 0, 0],
+    w = 164 - m[1] - m[3],
+    h = 120 - m[0] - m[2];
+
+    var
+    x = d3.time.scale().domain([startTime, endTime]).range([0, w]),
+    y = d3.scale.linear().domain([0, d3.max(data, function(d) { return d[1]; })]).range([h, 0]);
+
     // create a line function that can convert data[] into x and y points
     var line1 = d3.svg.line()
     // assign the X function to plot our line as we wish
@@ -159,10 +195,9 @@ Overlay.prototype = {
       return y(d[2]); // use the 2nd index of data (for example, get 13 from [20,13])
     })
 
-
     // Add an SVG element with the desired dimensions and margin.
-    var graph = d3.select("#"+divid).append("svg:svg")
-    .attr("width", w + m[1] + m[3])
+    var graph = d3.select("#" + divid).append("svg:svg")
+    .attr("width",  w + m[1] + m[3])
     .attr("height", h + m[0] + m[2])
     .append("svg:g")
     .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
@@ -172,27 +207,41 @@ Overlay.prototype = {
     graph.append("svg:path").attr("d", line1(data)).attr("class", "data1");
     graph.append("svg:path").attr("d", line2(data)).attr("class", "data2");
   },
+
   draw: function(map) {
-    var self = this;
-    var status = this.conflictmaps.getActiveStatus(this.time);
+
+    var
+    self   = this,
+    status = this.conflictmaps.getActiveStatus(this.time);
+
     var node = this.svg.selectAll("g")
-    .remove(status['inactive'])
+    .remove(status['inactive']);
+
     var node = this.svg.selectAll("g")
     .data(status['active'], function(d) { return d.id; })
     .attr('transform', function(val) {
-      var eq = val.data;
-      var p = eq.getPos(self.time);
+
+      var
+      eq = val.data,
+      p  = eq.getPos(self.time);
+
       p = map.coordinatePoint(map.locationCoordinate(p));
       return "translate(" + p.x + "," + p.y +")";
+
     })
     .enter()
     .append('g')
     .attr('transform', function(val) {
-      var eq = val.data;
-      var p = eq.getPos(self.time);
+
+      var
+      eq = val.data,
+      p  = eq.getPos(self.time);
+
       p = map.coordinatePoint(map.locationCoordinate(p));
       return "translate(" + p.x + "," + p.y +")";
+
     });
+
     node.append("circle")
     .attr('style', "fill: #00f; fill-opacity: 0.5");
 
@@ -204,19 +253,26 @@ Overlay.prototype = {
       var o = b.data.opacity(self.time);
       return "fill: #FF9900; fill-opacity: " + o + "; stroke-opacity: " + o;
     });
-    var offset = Math.ceil(262 * (self.time - this.conflictmaps.first().time.getTime()) / (this.conflictmaps.last().time.getTime() - this.conflictmaps.first().time.getTime()))
-    $('#progress').css('left', offset+"px");
+
+    var // calculate the offset
+    firstTime = this.conflictmaps.first().time.getTime(),
+    lastTime  = this.conflictmaps.last().time.getTime(),
+    offset    = Math.ceil(262 * (self.time - firstTime) / (lastTime - firstTime))
+
+    $('#progress').css('left', offset + "px");
   }
 }
 
 function initMap() {
-  var src = document.getElementById('src');
-  template = 'http://{S}tiles.mapbox.com/v3/cartodb.map-byl8dnag/{Z}/{X}/{Y}.png';
-  var subdomains = [ 'a.', 'b.', 'c.' ];
-  var provider = new MM.TemplatedLayer(template, subdomains);
+  var
+  src        = document.getElementById('src'),
+  template   = config.template,
+  subdomains = [ 'a.', 'b.', 'c.' ],
+  provider   = new MM.TemplatedLayer(template, subdomains);
 
   map = new MM.Map(document.getElementById('map'), provider);
   map.setCenterZoom(new MM.Location(config.lat, config.lng), config.zoom);
+
   var hash = new MM.Hash(map);
 }
 
@@ -230,30 +286,38 @@ function start(type) {
   var progress = document.getElementById('progress');
   progress.className = "show";
 
-  if (type=='replay'){
+  if (type == 'replay'){
+
     conflictmaps = new ConflictMaps();
 
     var setup_layer = function() {
-      var f = new Overlay(map, conflictmaps);
-      var to = 0;
-      var ai=null;
+
+      var
+      f  = new Overlay(map, conflictmaps);
+      to = 0,
+      ai = null;
+
       f.graph('graph')
+
       var moveMap = setInterval(function() {
 
         var of = f.time;
+
         f.setTime(30000000);
-        if (f.time<of){
+
+        if (f.time < of){
           f.time = this.conflictmaps.first().time.getTime();
         }
+
         f.draw(map);
       },20);
+
     };
 
-    // fetch all data
+    // Fetch all data
     conflictmaps.bind('reset', setup_layer);
     conflictmaps.fetch();
   }
-
 }
 
 
@@ -302,5 +366,4 @@ var Clock = Class.extend({
   }
 });
 
-var clock = new Clock();
-
+clock = new Clock();
